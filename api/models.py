92110f22c312851a0465add426f4af07c4aac12f
RUYANGA <ruyangam15@gmail.com>
2025-09-16 18:15:55 +0200

    new features

--- api/models.py
+++ api/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title =models.CharField(max_length=200)
-    content = models.TextField
+    content = models.TextField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.title
--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Post
 # Create your views here.
 
 @api_view(['GET'])
 def index(request):
     return Response({"Success":"The setup was successful"})
+
+@api_view(['GET'])
+def getAllPosts(request):
+   get_post=Post.objects.all()
+   return Response(get_post)
